b451648b3e0b255549bc69f19773462e41069722
Emilio <emiliodavidr@gmail.com>
2018-01-08 14:22:07 -0800

    megaphone and makefile

--- d00/ex00/megaphone.cpp
+++ d00/ex00/megaphone.cpp
@@ -6,14 +6,13 @@
 /*   By: eramirez <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/08 13:32:32 by eramirez          #+#    #+#             */
-/*   Updated: 2018/01/08 14:02:35 by eramirez         ###   ########.fr       */
+/*   Updated: 2018/01/08 14:21:18 by eramirez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <iostream>
 #include <string>
 int main (int argc, char **argv)
 {
-	char* str;
 	int i;
 	int j;
 	i = -1;
